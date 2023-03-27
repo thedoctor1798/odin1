7245c590d5b74d520619b7b8af4e4effe946a1e6
thedoctor1798 <olex981@gmail.com>
2023-03-27 17:43:27 +0200

    popup form, js barátkozás

---
+++ script.js
@@ -0,0 +1,6 @@
+function openForm() {
+    document.getElementById("contact-form").style.display = "block";
+  }
+  function closeForm() {
+    document.getElementById("contact-form").style.display = "none";
+  }
